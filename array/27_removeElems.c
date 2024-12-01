a2c6db15959f6d556a5b1f10e33f1188e08bc565
changan-memories <qinyuxiangzzu@outlook.com>
2024-12-01 13:04:04 +0800

    update

--- array/27_removeElems.c
+++ array/27_removeElems.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-// 时间复杂O(N)   空间复杂度为O(1)
 
+// 时间复杂O(N)   空间复杂度为O(1)
 int removeElement(int* nums, int numsSize, int val) {
     int src = 0, dst = 0;
     while (src < numsSize) {
@@ -15,6 +15,7 @@ int removeElement(int* nums, int numsSize, int val) {
     }
     return dst; // 数组中元素的下标  可以表示该元素之前的元素个数
 }
+
 int main() {
     int arr[] = {1, 2, 2, 0, 4, 5, 2, 3, 2};
     removeElement(arr, sizeof(arr) / sizeof(arr[0]), 2);
